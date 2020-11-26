bb718ef9d9c75edc46f42d06064aeb2862acce7e
rootyss <jaxent@yandex.ru>
2020-11-26 12:27:43 +0300

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export const getUserName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+
+
