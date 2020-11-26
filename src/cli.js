a93f104a23dd0a582b328d7614df8ed7ccc38c5b
rootyss <jaxent@yandex.ru>
2020-11-26 19:06:15 +0300

    asdf

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => {
+const getUserName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export { getUserName };
