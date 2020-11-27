846dbbec82b4d0956c8b4b5f8944d068dafe0c05
rootyss <jaxent@yandex.ru>
2020-11-27 18:56:59 +0300

    fixed linter

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import getUserName from '../src/cli.js';
 import readlineSync from 'readline-sync';
+import getUserName from '../src/cli.js';
 import { checkEven, giveCondition, getRandomNum } from '../games/checkEven.js';
 
 console.log('Welcome to the Brain Games!');
@@ -9,16 +9,16 @@ giveCondition();
 
 let countRightAnswers = 0;
 
-while(countRightAnswers < 3) {
-  let randomNum = getRandomNum(100);
-  let even = checkEven(randomNum);
+while (countRightAnswers < 3) {
+  const randomNum = getRandomNum(100);
+  const even = checkEven(randomNum);
   console.log(`Question: ${randomNum}`);
-  let answer = readlineSync.question('Your answer: ');
+  const answer = readlineSync.question('Your answer: ');
   if (answer === even) {
-    console.log(`Correct!`);
-  	countRightAnswers += 1;
+    console.log('Correct!');
+    countRightAnswers += 1;
   } else {
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${even}'.\nLet's try again, ${userName}!`)
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${even}'.\nLet's try again, ${userName}!`);
   }
 }
 if (countRightAnswers === 3) console.log(`Congratulations, ${userName}!`);
